2ea4a76f5967d3101b952490f597ad50e1b4ff06
kugimiya <bp.001@yandex.ru>
2022-10-06 02:11:36 +0400

    [+] Добавлены баннеры

--- src/componets/PageWrapper/index.tsx
+++ src/componets/PageWrapper/index.tsx
@@ -3,11 +3,30 @@ import { LINKS } from "src/constants";
 import { Board } from "src/types/board";
 import {Box} from "src/componets/Box"
 import Navbar from "src/componets/Navbar";
+import { randomInteger } from "src/utils/randomInteger";
 
 const bannersHrefs = [
   '/images/1.gif',
   '/images/1.png',
   '/images/2.png',
+  '/images/3.png',
+  '/images/4.png',
+  '/images/5.png',
+  '/images/6.jpg',
+  '/images/7.jpg',
+  '/images/8.jpg',
+  '/images/9.jpg',
+  '/images/10.jpg',
+  '/images/11.jpg',
+  '/images/12.jpg',
+  '/images/13.jpg',
+  '/images/14.jpg',
+  '/images/15.jpg',
+  '/images/16.jpg',
+  '/images/17.jpg',
+  '/images/18.jpg',
+  '/images/19.jpg',
+  '/images/20.jpg',
 ];
 
 export const PageWrapper = ({ boards, children }: { boards: Board[]; children: JSX.Element }) => {
@@ -19,7 +38,7 @@ export const PageWrapper = ({ boards, children }: { boards: Board[]; children: J
 
       <Box padding="8px" width="100%" flexDirection="column" style={{ maxWidth: 'calc(100% - 280px)' }}>
         <Box width="100%" justifyContent="center" style={{ marginBottom: '24px' }}>
-          <Image src={bannersHrefs[0]} alt="Banner" width={300} height={100} />
+          <Image src={bannersHrefs[randomInteger(0, bannersHrefs.length)] || bannersHrefs[0]} alt="Banner" width={300} height={100} />
         </Box>
 
         <Box>
--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,5 @@ export const API_URI = 'http://pissykaka.scheoble.xyz';
 export const LINKS = [
   { text: 'Pissychan', href: 'http://pissychan.oxore.net/' },
   { text: 'Discord', href: 'https://discord.gg/DhhjsVgXBG' },
+  { text: 'Исходники', href: 'https://github.com/U-Me-Chan' }
 ];
