fef021a60a54da47a1eda933bddc04537e7ad0f8
kugimiya <bp.001@yandex.ru>
2022-10-23 01:44:31 +0400

    [~] Радио подвинуто вправо

--- src/components/layouts/CommonLayout.tsx
+++ src/components/layouts/CommonLayout.tsx
@@ -1,10 +1,12 @@
 import { memo } from 'react';
 import { Box } from 'src/components/common/Box';
 import { Navbar } from 'src/components/lib/Navbar';
+import { RADIOS_LINKS } from 'src/constants';
 import { useAllBoards } from 'src/services';
 import { theme } from 'src/theme';
 import styled from 'styled-components';
 
+import { RadioPlayer } from '../lib/RadioPlayer';
 import { SubsContainer } from '../lib/SubsContainer';
 
 const MainContainer = styled(Box)`
@@ -42,13 +44,28 @@ export const CommonLayout = memo(function CommonLayoutMemoized({
       backgroundColor='colorBgPrimary'
       alignItems='flex-start'
     >
-      <NavbarContainer width='100%' maxWidth='256px' style={{}}>
+      <NavbarContainer width='100%' maxWidth='256px'>
         <Navbar boards={allBoardsData.data?.payload?.boards || []} />
       </NavbarContainer>
 
       <Box width='100%' maxWidth='1024px' flexDirection='column' flexGrow='1'>
         {children}
       </Box>
+
+      <NavbarContainer width='100%' maxWidth='256px' flexDirection='column' gap='10px'>
+        {RADIOS_LINKS.map(({ name, link, apiBasePath }) => (
+          <Box
+            key={`${name}-${link}`}
+            justifyContent='center'
+            width='100%'
+            border='colorBgSecondary'
+            borderRadius='4px'
+            overflow='hidden'
+          >
+            <RadioPlayer mount={name} url={link} apiBasePath={apiBasePath} />
+          </Box>
+        ))}
+      </NavbarContainer>
 
       <SubsContainer boards={allBoardsData.data?.payload.boards || []} />
     </MainContainer>
